0770130da58277d9c1b19bfba9773406651cd421
wanggang <wanggang3333@gmail.com>
2015-10-20 21:50:33 +0800

    start chapter5

---
+++ chapter5/optimization.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+import time 
+import random
+import math
+
+people = [('Seymour','BOS'),
+          ('Franny','DAL'),
+          ('Zooey','CAK'),
+          ('Walt','MIA'),
+          ('Buddy','ORD'),
+          ('Les','OMA')]
+
+#目的地：纽约的LaGuardia 机场
+destination='LGA'
